837711daf68703efee58ea54b579f1b706088a87
girvel <widauka@ya.ru>
2019-08-30 14:41:06 +0500

    Write helloworld

---
+++ src/dying_space.py
@@ -0,0 +1,8 @@
+from tkinter import Tk, Label
+
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry("640x480")
+    root.title("Dying space")
+    Label(root, text="Hello, world!").pack()
+    root.mainloop()
