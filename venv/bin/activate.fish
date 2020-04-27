b8b5ac4bdc9145d9db5f0dfa8dd0f3ed2316109a
girvel <widauka@ya.ru>
2020-04-27 15:23:20 +0500

    #

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nik/PycharmProjects/DyingSpace/venv"
+set -gx VIRTUAL_ENV "/media/nik/865ED0925ED07C7D/projects/DyingSpace/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
